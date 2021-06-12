3f6acc47aa6ed58e46c00ee27ef1b33ee4befde8
SubhraBaranwal <85775851+SubhraBaranwal@users.noreply.github.com>
2021-06-12 09:59:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+// in sketch.js :: main program
+// createSprite
+//drawSprite
+//p5.js: library
+
+//ctrl+s
+//cmd+s
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  rect(200,200,40,70);
+  rect(100,100,40,70);
+}
